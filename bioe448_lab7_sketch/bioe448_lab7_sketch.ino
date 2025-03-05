11102ebef9c53955d8a23ba96417a3cb47725864
unknown <bisharabrian2@gmail.com>
2025-03-05 14:07:44 -0600

    added iot functionality

--- bioe448_lab7_sketch/bioe448_lab7_sketch.ino
+++ bioe448_lab7_sketch/bioe448_lab7_sketch.ino
@@ -1,21 +1,39 @@
-const int sensor_pin = 0;
-int pulse_signal = 0;
-float pulse_period = 0;
-float BPM = 0;
+#include "thingProperties.h"
 
-// declare flags for data storage
-bool any_peak_detected = false;
-bool first_peak_detected = false;
+int sensor_pin = 0;
+int pulse_signal = 0;
+int counter = 0;
+//float BPM = 0.0;
+
+bool ignore = false;
+bool first_pulse_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
+unsigned long pulse_period = 0;
+bool any_peak_detected = false;
+bool first_peak_detected = false;
 
 // define thresholds
-int upper_threshold = 340;
-int lower_threshold = 320;
+int upper_threshold = 950;
+int lower_threshold = 700;
 
 void setup() {
 
   Serial.begin(9600);
+
+  delay(1500);
+  initProperties();
+
+  //Connect to cloud and get info/errors
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+  
+  //Wait for cloud connection
+  while (ArduinoCloud.connected() != 1) {
+    ArduinoCloud.update();
+    delay(500);
+  }
 
 }
 
@@ -54,5 +72,12 @@ void loop() {
   // calculate BMP from pulse period
   BPM = 60000/pulse_period;
   Serial.println(BPM);
+
+  counter++;
+  if (counter > 200){
+    ArduinoCloud.update();
+    Serial.println(BPM);
+    counter = 0;
+  }
 
 }
